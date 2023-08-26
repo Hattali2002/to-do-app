e08623d0ecb9ccd0e8e280a7cdaf191d3eaa8274
Hattali2002 <shivanandhattali2002@gmail.com>
2023-08-27 00:04:23 +0530

    refactored the font size

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './style.css'
 
-function App() {
+export default function App() { 
+  const [article, setArticle] = useState([]);
+  const [data, setData] = useState("");
+
+  const changeData = (event) => {
+    setData(event.target.value);
+  }
+  
+  const submit = () => {
+    if (data.trim() !== "") {
+      setArticle(article.concat(data));
+      setData("")
+    }
+  }
+
+  const filter = (element) => {
+    const newdata = article.filter(item => item !== element);
+    setArticle(newdata);
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="heading my-4">
+        <h3>TO DO LIST</h3>
+      </div>
+      <div className="main my-4">
+        <div className="input">
+          <form>
+            <input type="text" placeholder='Add Item' value={data} onChange={changeData} />
+          </form>
+          <button className='button mx-4' name='add' onClick={submit}>
+            <i className="fa-thin fa-plus"></i>
+          </button>
+        </div>
+        <div className="content">
+          {article.map((element, index) => {
+            return <div key={index} className='my-4'>
+              {element}
+              <button className='mx-4 buttonDel' name='delete' onClick={()=>{filter(element)}}>
+              <i class="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          })}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
